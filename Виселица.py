3f20b50958bef2e95a978282b669b5f589a0e238
SergionJuice <98183928+SergionJuice@users.noreply.github.com>
2022-02-05 12:45:11 +0300

    Add files via upload

---
+++ Виселица.py
@@ -0,0 +1,40 @@
+from ntpath import join
+from operator import index
+
+
+def hangman (word):
+    wrong = 0
+    stages = ["",
+             "________      "
+             "|      |  ",
+             "|      0  ",
+             "|     /|\ ",
+             "|     / \ ",
+             "|          "
+    rletters = list (word)
+    board = ["_"] * len (word)
+    win = False
+    print ("Добро пожаловать на казнь")
+while wrong<lem(stages) - 1:
+    print ("\n")
+    msg ="Введите букву"
+    char = input(msg)
+    if char in rletters:
+        cind = rletters.index(char)
+        board[cind] =char
+        rletters[cind] = '$'
+    else:
+        wrong += 1
+    print ((" ".join(board)))
+    e = wrong + 1 
+    print ("\n".join(stages [0:e]))
+    if "_" not in board:
+        print ("Вы выйграли!")
+        win = True
+        break
+if not win:
+    print ("\n".join(stages[0:wrong]))
+    print ("Вы проиграли")
+
+hangman ("кот")
+    
